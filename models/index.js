4b9ae5ff39a1c02ff6cafbdaaafe786383af4fff
mich-hales <michellehales93@gmail.com>
2022-10-21 16:45:52 -0600

    setting up user.js data in models.

---
+++ models/index.js
@@ -0,0 +1,15 @@
+const User = require('./User');
+const Post = require('./Post');
+const Comment = require('./Comment');
+
+User.hasMany(Post, {
+    foreignKey: 'user_id'
+});
+
+User.hasMany(Comment, {
+    foreignKey: 'user_id'
+});
+
+Post.belongsTo(User, {
+    foreignKey: 'user_id'
+})
