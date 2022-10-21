35c625f376d0fec3d3639dcfafec9726e90fb53b
mich-hales <michellehales93@gmail.com>
2022-10-21 17:20:11 -0600

    layout for index.js file

--- models/index.js
+++ models/index.js
@@ -3,13 +3,30 @@ const Post = require('./Post');
 const Comment = require('./Comment');
 
 User.hasMany(Post, {
-    foreignKey: 'user_id'
+    foreignKey: 'user_id',
+    onDelete: 'SET NULL'
 });
 
 User.hasMany(Comment, {
-    foreignKey: 'user_id'
+    foreignKey: 'user_id',
+    onDelete: 'SET NULL'
 });
 
 Post.belongsTo(User, {
     foreignKey: 'user_id'
-})
+});
+
+Post.hasMany(Comment, {
+    foreignKey: 'post_id',
+    onDelete: 'CASCADE'
+});
+
+Comment.belongsTo(User, {
+    foreignKey: 'user_id'
+});
+
+Comment.belongsTo(Post, {
+    foreignKey: 'post_id'
+});
+
+module.exports = { User, Post, Comment };
