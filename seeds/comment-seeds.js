05ee4c77f0e87b75150cec274e3ce30cd0b2d916
mich-hales <michellehales93@gmail.com>
2022-11-03 17:18:06 -0600

    tech blog

--- seeds/comment-seeds.js
+++ seeds/comment-seeds.js
@@ -5,42 +5,42 @@ const commentData = [
         user_id: '2',
         post_id: '1',
         comment: 'Wow so cool!',
-        date: 10/11/22,
+        date: 10/11/2022,
         
     },
     {
         user_id: '3',
         post_id: '2',
         comment: 'Interesting.',
-        date: 10/01/22,
+        date: 10/01/2022,
         
     },
     {
         user_id: '1',
         post_id: '3',
         comment: 'I never knew that.',
-        date: 9/18/22,
+        date: 9/18/2022,
         
     },
     {
         user_id: '3',
         post_id: '4',
         comment: 'Would love your contact info.',
-        date: 8/11/22,
+        date: 8/11/2022,
         
     },
     {
         user_id: '2',
         post_id: '5',
         comment: 'What prompted this blog post?',
-        date: 6/30/22,
+        date: 6/30/2022,
         
     },
     {
         user_id: '1',
         post_id: '6',
         comment: 'really cool',
-        date: 5/4/22,
+        date: 5/4/2022,
         
     }
 ];
--- seeds/post-seeds.js
+++ seeds/post-seeds.js
@@ -4,31 +4,31 @@ const postData = [
     {
         title: 'Tech Savvy',
         content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce cursus diam eget nunc placerat vehicula. Pellentesque eget turpis sed neque dignissim mattis. Duis lacinia, lectus vitae accumsan hendrerit, metus elit iaculis magna, quis dignissim nisi dolor ut nibh. Maecenas vitae magna quis est consectetur tempus et vitae tellus. Nunc turpis velit, vulputate a magna eu, dignissim condimentum enim. ',
-        date: 10/12/22,
+        date: 10/12/2022,
         user_id: 1,
     },
     {
         title: 'Back to Basics',
         content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce cursus diam eget nunc placerat vehicula. Pellentesque eget turpis sed neque dignissim mattis. Duis lacinia, lectus vitae accumsan hendrerit, metus elit iaculis magna, quis dignissim nisi dolor ut nibh. Maecenas vitae magna quis est consectetur tempus et vitae tellus. Nunc turpis velit, vulputate a magna eu, dignissim condimentum enim. ',
-        date: 9/12/22,
+        date: 9/12/2022,
         user_id: 2,
     },
     {
         title: 'Where to Begin Your Tech Journey',
         content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce cursus diam eget nunc placerat vehicula. Pellentesque eget turpis sed neque dignissim mattis. Duis lacinia, lectus vitae accumsan hendrerit, metus elit iaculis magna, quis dignissim nisi dolor ut nibh. Maecenas vitae magna quis est consectetur tempus et vitae tellus. Nunc turpis velit, vulputate a magna eu, dignissim condimentum enim. ',
-        date: 10/02/22,
+        date: 10/02/2021,
         user_id: 3,
     },
     {
         title: 'Coding Secrets Every Developer Should Know',
         content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce cursus diam eget nunc placerat vehicula. Pellentesque eget turpis sed neque dignissim mattis. Duis lacinia, lectus vitae accumsan hendrerit, metus elit iaculis magna, quis dignissim nisi dolor ut nibh. Maecenas vitae magna quis est consectetur tempus et vitae tellus. Nunc turpis velit, vulputate a magna eu, dignissim condimentum enim. ',
-        date: 1/12/22,
+        date: 1/12/2020,
         user_id: 2,
     },
     {
         title: 'Advanced Tech Interview Prep Questions',
         content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce cursus diam eget nunc placerat vehicula. Pellentesque eget turpis sed neque dignissim mattis. Duis lacinia, lectus vitae accumsan hendrerit, metus elit iaculis magna, quis dignissim nisi dolor ut nibh. Maecenas vitae magna quis est consectetur tempus et vitae tellus. Nunc turpis velit, vulputate a magna eu, dignissim condimentum enim. ',
-        date: 3/12/22,
+        date: 3/12/2019,
         user_id: 1,
     },
 ]
